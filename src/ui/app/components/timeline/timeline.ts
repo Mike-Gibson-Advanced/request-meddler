be5d6f8dffd081813dc1ff122af6df1df016d70e
Mike Gibson <mike.gibson@oneadvanced.com>
2017-11-03 17:09:11 +0000

    Rough filtering on timeline

--- src/ui/app/components/timeline/timeline.ts
+++ src/ui/app/components/timeline/timeline.ts
@@ -19,6 +19,27 @@ export class TimelineComponent extends Vue {
         return 30;
     }
 
+    get filterText() {
+        return `${this.customRegex ? `Requests with URL matching '${this.regex}'` : "All requests" } ` +
+            `in the last ${this.thresholdInSeconds} seconds`;
+    }
+
+    get customRegex(): RegExp | null {
+        if (!this.regex) {
+            return null;
+        }
+
+        try {
+            return new RegExp(this.regex);
+        } catch (error) {
+            return null;
+        }
+    }
+
+    showFilter: boolean = false;
+    regex: string = "";
+    regexError: boolean = false;
+
     visibleRequests: IRequest[] = [];
     min: Date = new Date();
     max: Date = new Date();
@@ -30,11 +51,28 @@ export class TimelineComponent extends Vue {
         this.refreshData();
     }
 
+    regexChanged(newValue: string) {
+        this.regex = newValue;
+        try {
+            new RegExp(newValue);
+            this.regexError = false;
+            this.refreshData();
+        } catch (error) {
+            this.regexError = true;
+        }
+    }
+
     async created() {
         try {
             await this.$store.dispatch("loadRequests");
         } catch (error) {
             alert("Error loading requests");
+        }
+
+        try {
+            await this.$store.dispatch("loadRules");
+        } catch (error) {
+            alert("Error loading rules");
         }
 
         this.intervalId = setInterval(() => {
@@ -52,7 +90,8 @@ export class TimelineComponent extends Vue {
         this.max = new Date(now.getTime());
         this.visibleRequests = this.allRequests.filter((r) => {
             return r.request.time <= this.max &&
-                ((r.response && r.response.time) || now) >= this.min;
+                ((r.response && r.response.time) || now) >= this.min &&
+                (this.customRegex || new RegExp(".*")).test(r.url);
         });
     }
 }
